886d1657393364613e16d1922f47932e395e34a5
marklai1998 <marklai1998@gmail.com>
2022-10-08 00:07:32 +0800

    feat: accurate line color

--- src/constants/line.ts
+++ src/constants/line.ts
@@ -15,51 +15,51 @@ export const lineConfig = {
   [Line.TML]: {
     nameEn: 'Tuen Ma Line',
     nameZh: '屯碼綫',
-    color: '#5d4037',
+    color: '#9A3820',
   },
   [Line.EAL]: {
     nameEn: 'East Rail Line',
     nameZh: '東鐵線',
-    color: '#2C5282',
+    color: '#61B4E4',
   },
   [Line.TWL]: {
     nameEn: 'Tsuen Wan Line',
     nameZh: '荃灣綫',
-    color: '#9B2C2C',
+    color: '#E2231A',
   },
   [Line.TCL]: {
     nameEn: 'Tung Chung Line',
     nameZh: '東涌綫',
-    color: '#DD6B20',
+    color: '#F38B00',
   },
   [Line.TKL]: {
     nameEn: 'Tseung Kwan O Line',
     nameZh: '將軍澳綫',
-    color: '#553C9A',
+    color: '#A35EB5',
   },
   [Line.AEL]: {
     nameEn: 'Airport Express',
     nameZh: '機場快綫',
-    color: '#285E61',
+    color: '#007078',
   },
   [Line.KTL]: {
     nameEn: 'Kwun Tong Line',
     nameZh: '觀塘綫',
-    color: '#276749',
+    color: '#00AF41',
   },
   [Line.DRL]: {
     nameEn: 'Disneyland Resort Line',
     nameZh: '迪士尼綫',
-    color: '#97266D',
+    color: '#E777CB',
   },
   [Line.ISL]: {
     nameEn: 'Island Line',
     nameZh: '港島綫',
-    color: '#2C5282',
+    color: '#0071CE',
   },
   [Line.SIL]: {
     nameEn: 'South Island Line',
     nameZh: '南港島綫',
-    color: '#AFB42B',
+    color: '#B6BD00',
   },
 }
