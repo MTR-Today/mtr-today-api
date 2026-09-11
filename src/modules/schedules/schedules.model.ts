e527ad325720e3e43ceebddb647bc212cce6afde
marklai1998 <marklai1998@gmail.com>
2026-09-10 23:09:52 -0400

    fix: string platform

--- src/modules/schedules/schedules.model.ts
+++ src/modules/schedules/schedules.model.ts
@@ -1,10 +1,10 @@
-import { Field, Int, ObjectType } from '@nestjs/graphql';
+import { Field, ObjectType } from '@nestjs/graphql';
 import { LineCode, StopCode } from 'mtr-kit';
 
 @ObjectType()
 export class ScheduleItem {
-  @Field(() => Int)
-  platform!: number;
+  @Field()
+  platform!: string;
 
   @Field()
   destination!: string;
